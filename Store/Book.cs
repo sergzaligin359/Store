d885df4c12b51c8db9bafe392ac6a3fca1f18403
sergzaligin359 <webdevjsphp@gmail.com>
2020-12-26 17:41:29 +0300

    Добавить реализацию репозитория книг в памяти

---
+++ Store/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Store
+{
+    public class Book
+    {
+        /*
+        * Это не поле, а свойство с модификатором get т е только на чтение 
+        */
+        public int Id { get; }
+        /*
+        * Это не поле, а свойство с модификатором get т е только на чтение 
+        */
+        public string Title { get; }
+
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C++ for 24 hours"),
+            new Book(4, "JavaScript ninja!!!"),
+            new Book(5, "History programming")
+
+        };
+        public Book[] GetAllByTitle(string titlePath)
+        {
+            return books.Where(book => book.Title.Contains(titlePath))
+                    .ToArray();
+        }
+    }
+}
